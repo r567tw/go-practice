1dba2b3322b71c32ccd0381e4234018a6af47dc0
r567tw <r567tw@gmail.com>
2021-02-03 22:51:02 +0800

    array

---
+++ HelloWorld/array.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// 第一種
+	var todos [2]string
+	todos[0] = "learning go !"
+	todos[1] = "use go to write an app"
+	fmt.Println(todos[1])
+	// 第二種
+	var grades [3]int = [3]int{90, 98, 93}
+	fmt.Println(grades[2])
+	// 第三種
+	heights := [3]int{123, 8, 93}
+	fmt.Println(heights[0])
+}
